e7cba3330e0b721a3b0325d300a319282ad91abc
Kapelusz20 <45516276+Kapelusz20@users.noreply.github.com>
2018-12-01 16:42:06 +0100

    Add files via upload

---
+++ Cwiczenie3.java
@@ -0,0 +1,29 @@
+public class Cwiczenie3 {
+    public static void main(String[] args) {
+
+        //1.
+
+       /* int x = 14_000; // x = InvestmentValue
+        x *= 1.4;
+        x -= 1500;
+        x *=1.12;
+
+        System.out.println(x);*/
+
+        //2.
+
+      /*  int a = 6;
+        int b = 8;
+
+        System.out.println(b / a + "\t" + b % a );*/
+
+      //3.
+
+        /*int x = 12;
+
+        System.out.println("Binary: " + Integer.toBinaryString(x));
+        System.out.println("Octal: " + Integer.toOctalString(x));
+        System.out.println("Hexal: " + Integer.toHexString(x));*/
+
+    }
+}
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,5 @@
-class HelloWorld{
-  public static void main(String[] args);
-  System.out.println("HelloWorld!");
+public class HelloWorld {
+        public static void main(String[] args) {
+                System.out.println("Hello World!");
+        }
 }
